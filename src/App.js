028a2698616a0358ad545056dfad63e61db57e34
niniwoo <qhdk6914@naver.com>
2024-05-16 16:25:26 -0700

    fixed menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Brand from "./components/Body/Brand";
+import FixedMenu from "./components/FixedMenu";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
       <Brand />
+      <FixedMenu />
     </div>
   );
 }
---
+++ src/components/FixedMenu.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FixedMenu = () => {
+  return (
+    <div>
+      <div className="flex flex-col space-y-10">
+        <div className="shadow-xl rounded-lg">Contact us</div>
+        <div className="shadow-xl rounded-lg">Customer support</div>
+        <div className="shadow-xl rounded-lg">Free consultation</div>
+      </div>
+    </div>
+  );
+};
+
+export default FixedMenu;
