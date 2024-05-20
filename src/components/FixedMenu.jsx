bbb8a240237ab3451aa02665c20650eb577ea130
niniwoo <qhdk6914@naver.com>
2024-05-19 17:15:40 -0700

    created floating menu

--- src/components/FixedMenu.jsx
+++ src/components/FixedMenu.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 const FixedMenu = () => {
   return (
-    <div>
-      <div className="flex flex-col space-y-10">
-        <div className="shadow-xl rounded-lg">Contact us</div>
-        <div className="shadow-xl rounded-lg">Customer support</div>
-        <div className="shadow-xl rounded-lg">Free consultation</div>
+    <div className="fixed z-5 top-0 left-0 w-full h-full flex flex-row-reverse items-end">
+      <div className="flex flex-col space-y-3 w-44 mr-10 right-0">
+        <div className="bg-white shadow-xl rounded-2xl p-2 border text-orange-500">
+          Contact us
+        </div>
+        <div className="bg-white shadow-xl rounded-2xl p-2 border">
+          Customer support
+        </div>
+        <div className="bg-white shadow-xl rounded-2xl p-2 border">
+          Free consultation
+        </div>
       </div>
     </div>
   );
