8169fcbd402a53c21da1bfd90a13fc395f11b796
niniwoo <qhdk6914@naver.com>
2024-05-23 16:58:18 -0700

    adding margin bottom for fixmenu

--- src/components/FixedMenu.jsx
+++ src/components/FixedMenu.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const FixedMenu = () => {
   return (
     <div className="fixed z-5 top-0 left-0 w-full h-full flex flex-row-reverse items-end">
-      <div className="flex flex-col space-y-3 w-44 mr-10 right-0">
+      <div className="flex flex-col space-y-3 w-44 mr-10 right-0 mb-10">
         <div className="bg-white shadow-xl rounded-2xl p-2 border text-orange-500">
           Contact us
         </div>
